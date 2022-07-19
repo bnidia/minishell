e309a519186d774199cfd5244b162bb78df50835
Andrey Baluev <owspdopf@mbowspdopf.local>
2022-07-19 18:25:45 +0300

    minimal lexer

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,12 +6,31 @@
 /*   By: fdonna <fdonna@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 18:01:53 by fdonna            #+#    #+#             */
-/*   Updated: 2022/07/12 19:39:45 by fdonna           ###   ########.fr       */
+/*   Updated: 2022/07/19 18:12:24 by fdonna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "token.h"
 #include "libft.h"
+
+char	*ft_strcopy(const char *src)
+{
+	int		len;
+	char	*copy;
+	int		i;
+
+	i = 0;
+	len = ft_strlen(src);
+	copy = ft_calloc(1, (t_size)len + 1);
+	if (copy == (char *)0)
+		return ((char *)0);
+	while (src[i] != '\0')
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	return (copy);
+}
 
 // #[allocation]
 static t_token *token_new(char *str, e_token_type type)
@@ -32,25 +51,111 @@ static void	lex_skip_whitespaces(t_cmdline *cmdline)
 
 static t_token *lex_quote(t_cmdline *cmdline)
 {
-	(void) cmdline;
+	char	value[512];
+	int		i;
+
+	i = 0;
+	ft_memset((void *)value, '\0', 512);
+	if (*cmdline->begin == '\'')
+	{
+		cmdline->begin++;
+		while (*cmdline->begin != '\'')
+		{
+			if (*cmdline->begin == '\0')
+				return ((t_token *)0);
+			value[i++] = *cmdline->begin;
+			cmdline->begin++;
+		}
+		cmdline->begin++;
+		lex_skip_whitespaces(cmdline);
+		return (token_new(ft_strcopy(value), TOKEN_TYPE_QUOTE));
+	}
+	if (*cmdline->begin == '\"')
+	{
+		cmdline->begin++;
+		while (*cmdline->begin != '\"')
+		{
+			if (*cmdline->begin == '\0')
+				return ((t_token *)0);
+			value[i++] = *cmdline->begin;
+			cmdline->begin++;
+		}
+		cmdline->begin++;
+		lex_skip_whitespaces(cmdline);
+		return (token_new(ft_strcopy(value), TOKEN_TYPE_QUOTE_DOUBLE));
+	}
 	return ((t_token *)0);
 }
 
 static t_token *lex_pipe(t_cmdline *cmdline)
 {
-	(void) cmdline;
-	return ((t_token *)0);
+	cmdline->execed = EXECED_NO;
+	cmdline->begin++;
+	lex_skip_whitespaces(cmdline);
+	return (token_new((char *)0, TOKEN_TYPE_PIPE));
 }
 
 static t_token *lex_less(t_cmdline *cmdline)
 {
-	(void) cmdline;
+	int		double_less;
+	char	input[255];
+	int		i;
+
+	i = 0;
+	double_less = 0;
+	ft_memset((void *)input, '\0', 255);
+	if (*cmdline->begin == '<')
+	{
+		cmdline->begin++;
+		if (*cmdline->begin == '<')
+		{
+			double_less = 1;
+			cmdline->begin++;
+		}
+		lex_skip_whitespaces(cmdline);
+		while (*cmdline->begin != ' ' && *cmdline->begin != '\t')
+		{
+			input[i++] = *cmdline->begin;
+			cmdline->begin++;
+		}
+		lex_skip_whitespaces(cmdline);
+		if (double_less)
+			return (token_new(ft_strcopy(input), TOKEN_TYPE_LESS_DOUBLE));
+		else
+			return (token_new(ft_strcopy(input), TOKEN_TYPE_LESS));
+	}
 	return ((t_token *)0);
 }
 
 static t_token *lex_more(t_cmdline *cmdline)
 {
-	(void) cmdline;
+	int		double_more;
+	char	output[255];
+	int		i;
+
+	i = 0;
+	double_more = 0;
+	ft_memset((void *)output, '\0', 255);
+	if (*cmdline->begin == '>')
+	{
+		cmdline->begin++;
+		if (*cmdline->begin == '>')
+		{
+			double_more = 1;
+			cmdline->begin++;
+		}
+		lex_skip_whitespaces(cmdline);
+		while (*cmdline->begin != ' ' && *cmdline->begin != '\t' && *cmdline->begin != '\0')
+		{
+			output[i++] = *cmdline->begin;
+			cmdline->begin++;
+		}
+		lex_skip_whitespaces(cmdline);
+		if (double_more)
+			return (token_new(ft_strcopy(output), TOKEN_TYPE_MORE_DOUBLE));
+		else
+			return (token_new(ft_strcopy(output), TOKEN_TYPE_MORE));
+	}
 	return ((t_token *)0);
 }
 
@@ -60,13 +165,23 @@ static t_token *lex_more(t_cmdline *cmdline)
 // lex_pipe(), lex_more(), lex_less()
 static t_token *lex_current_line(t_cmdline *cmdline)
 {
+	char	value[512];
+	int		i;
+
+	i = 0;
+	ft_memset((void *)value, '\0', 512);
 	lex_skip_whitespaces(cmdline);
 	if (cmdline->execed == EXECED_NO) // then we handle new execution command like [ls] -la ..
 	{
 		if (*cmdline->begin == '\'' || *cmdline->begin == '\"') // we can't exec "ls" -la
 			return ((t_token *)0);
 		cmdline->execed = EXECED_YES;
-		return (token_new((char *)0, TOKEN_TYPE_EXEC));
+		while (*cmdline->begin != ' ' && *cmdline->begin != '\t' && *cmdline->begin != '\0')
+		{
+			value[i++] = *cmdline->begin;
+			cmdline->begin++;
+		}
+		return (token_new(ft_strcopy(value), TOKEN_TYPE_EXEC)); // return exec str
 	}
 	if (*cmdline->begin == '\'' || *cmdline->begin == '\"')
 		return (lex_quote(cmdline));
@@ -76,14 +191,19 @@ static t_token *lex_current_line(t_cmdline *cmdline)
 		return (lex_more(cmdline));
 	else if (*cmdline->begin == '<')
 		return (lex_less(cmdline));
-	return (token_new((char *)0, TOKEN_TYPE_ARG));
+	while (*cmdline->begin != ' ' && *cmdline->begin != '\t' && *cmdline->begin != '\0')
+	{
+		value[i++] = *cmdline->begin;
+		cmdline->begin++;
+	}
+	return (token_new(ft_strcopy(value), TOKEN_TYPE_ARG)); // return arg str
 }
 
 // #[desc] handle end or newline
 static t_token *lex_next_token(t_cmdline *cmdline)
 {
 	lex_skip_whitespaces(cmdline);
-	if (cmdline->begin == cmdline->end)
+	if (*cmdline->begin == '\0')
 		return (token_new((char *)0, TOKEN_TYPE_END));
 	if (*cmdline->begin == '\\' && \
 	(*(cmdline->begin + 1) == ' ' || *(cmdline->begin + 1) == '\t'))
@@ -107,6 +227,7 @@ t_token_list *lex_tokenize(t_cmdline cmdline)
 		current = token_new((char *)0, TOKEN_TYPE_BEGIN);
 		if (current != (t_token *)0)
 		{
+			ft_lstadd_back(&token_list_ptr->tokens, ft_lstnew((void *)current));
 			while (current->type != TOKEN_TYPE_END)
 			{
 				current = lex_next_token(&cmdline);
@@ -115,6 +236,8 @@ t_token_list *lex_tokenize(t_cmdline cmdline)
 					token_list_free(token_list_ptr);
 					return ((t_token_list *)0);
 				}
+				else
+					ft_lstadd_back(&token_list_ptr->tokens, ft_lstnew((void *)current));
 			}
 			return (token_list_ptr);
 		}
--- src/minimain.c
+++ src/minimain.c
@@ -6,16 +6,45 @@
 /*   By: fdonna <fdonna@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 16:52:49 by fdonna            #+#    #+#             */
-/*   Updated: 2022/07/12 18:17:51 by fdonna           ###   ########.fr       */
+/*   Updated: 2022/07/19 18:24:20 by fdonna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "token.h"
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+void	print_tokens(void *data)
+{
+	t_token	*token_ptr;
+
+	token_ptr = (t_token *)data;
+	printf("token { type: %2d, str: %s }\n", token_ptr->type, token_ptr->str);
+}
 
 int minimain(t_env_list *envs)
 {
+	char			*line;
+	t_token_list	*token_list;
+	t_cmdline		cmdline;
+
 	(void) envs;
-	// if (something)
-	// 	return (MINIMAIN_EXIT);
+	line = readline("#> ");
+	if (line == (char *)0)
+		return (MINIMAIN_EXIT);
+
+	cmdline = (t_cmdline) {
+		.str	= line,
+		.begin	= line,
+		.end	= line + ft_strlen(line),
+		.size	= ft_strlen(line),
+		.execed	= EXECED_NO
+	};
+
+	token_list = lex_tokenize(cmdline);
+	ft_lstiter(token_list->tokens, print_tokens);
+	token_list_free(token_list);
 	return (MINIMAIN_CONTINUE);
 }
